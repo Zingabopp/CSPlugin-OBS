e3b51d821f9fe04fcd40c838811ed55ae568d7d6
Zingabopp <37518403+Zingabopp@users.noreply.github.com>
2019-03-04 03:19:37 -0600

    Add project files.

---
+++ CSPlugin-OBS/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSPlugin_OBS
+{
+    public class Class1
+    {
+    }
+}
